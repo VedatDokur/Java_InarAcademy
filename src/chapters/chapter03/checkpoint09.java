f601425155670ccbd0c008ece13c1b115e11b977
VedatDokur <vedatdokurr7@gmail.com>
2022-08-30 23:39:27 +0200

    checkpoints of chapter03

---
+++ src/chapters/chapter03/checkpoint09.java
@@ -0,0 +1,22 @@
+package chapters.chapter03;
+
+public class checkpoint09 {
+
+	public static void main(String[] args) {
+		//If Else Mistake Examples.
+		
+		int score = 83;
+		if (score >= 90.0)
+			System.out.println("A");
+		else if (score >= 80.0)
+			System.out.println("B");
+		else if (score >= 70.0)
+			System.out.println("C");
+		else if (score >= 60.0)
+			System.out.println("D");
+		else
+			System.out.println("F");
+
+	}
+
+}

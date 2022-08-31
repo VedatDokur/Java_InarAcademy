8daff58d654a35593482bd7f4e97a9e61dc7452c
VedatDokur <vedatdokurr7@gmail.com>
2022-08-31 23:59:56 +0200

    added chapter03 exercises

---
+++ src/chapters/chapter03/exercise16.java
@@ -0,0 +1,13 @@
+package chapters.chapter03;
+
+public class exercise16 {
+	public static void main(String[] args) {
+	// Random point.
+
+			double x = Math.random() * 100 - 50;
+			double y = Math.random() * 200 - 100;
+
+			System.out.println(x + "," + y);
+		}
+}
+
---
+++ src/chapters/chapter03/exercise17.java
@@ -0,0 +1,39 @@
+package chapters.chapter03;
+
+import java.util.Scanner;
+
+public class exercise17 {
+	public static void main(String[] args) {
+		
+		// Game: scissor, rock, paper.
+
+		Scanner input = new Scanner(System.in);
+		int number = (int) (((Math.random() * 100) / 33.34));
+		System.out.println(" Please enter scissor (0), rock (1), paper (2):");
+		int guess = input.nextInt();
+		if (number == guess && number == 0) {
+			System.out.println("The computer is scissor. You are scissor too. It is a draw");
+		} else if (number == guess && number == 1) {
+			System.out.println("The computer is rock. You are rock too. It is a draw");
+		} else if (number == guess && number == 2) {
+			System.out.println("The computer is paper. You are paper. It is a draw");
+		} else if (number == 0 && guess == 1) {
+			System.out.println("The computer is scissor. You are rock. You won.");
+
+		} else if (number == 0 && guess == 2) {
+			System.out.println("The computer is scissor. You are paper. You lost.");
+
+		} else if (number == 1 && guess == 0) {
+			System.out.println("The computer is rock. You are scissor. You lost.");
+
+		} else if (number == 1 && guess == 2) {
+			System.out.println("The computer is rock. You are paper. You won.");
+
+		} else if (number == 2 && guess == 0) {
+			System.out.println("The computer is paper. You are scissor. You won.");
+
+		} else if (number == 2 && guess == 1) {
+			System.out.println("The computer is paper. You are scissor. You lost.");
+		}
+	}
+}

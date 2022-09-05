6d80a02ec2f3ca5e5066102176df87b82f9ee597
VedatDokur <vedatdokurr7@gmail.com>
2022-09-05 23:54:13 +0200

    added chapter04 exercises

--- src/chapters/chapter04/exercise13.java
+++ src/chapters/chapter04/exercise13.java
@@ -1,38 +1,35 @@
 package chapters.chapter04;
 
-import java.util.Scanner;
-
-public class exericse13 {
+public class exercise13 {
 
 	public static void main(String[] args) {
-		//Vowel or consonant?
-		/* Enter a letter : B
-		 * B is a consonant
-		 * Enter a letter grade : A
-		 * A is a vowel
-		 * Enter a letter grade : #
-		 * İs an invalid input */
-		
+		// Vowel or consonant?
+		/*
+		 * Enter a letter : B B is a consonant Enter a letter grade : A A is a vowel
+		 * Enter a letter grade : # İs an invalid input
+		 */
+
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter a letter: ");
 		String s = input.nextLine();
-		
-		//Check your string for the one character.
-		if(s.length() != 1) {
+
+		// Check your string for the one character.
+		if (s.length() != 1) {
 			System.out.println("You must enter one character");
 			System.exit(1);
 		}
-		//Check if input is valid
+		// Check if input is valid
 		char ch = Character.toUpperCase(s.charAt(0));
-		if ( ch > 'Z' || ch < 'A') {
+		if (ch > 'Z' || ch < 'A') {
 			System.out.println(s + " is an invalid input");
 			System.exit(1);
 		}
-		if(ch == 'A' || ch == 'E' || ch == 'I' || ch == '0' | ch == 'U') {
+		if (ch == 'A' || ch == 'E' || ch == 'I' || ch == '0' | ch == 'U') {
 			System.out.println(s + " is a vowel");
 			System.exit(1);
 		}
 		System.out.println(s + " is a consonant");
+
 	}
 
 }

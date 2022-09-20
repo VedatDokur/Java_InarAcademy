194b114d347f7b80a62712e7fd85e3b2462cc2a6
VedatDokur <vedatdokurr7@gmail.com>
2022-09-20 23:56:57 +0200

    added chapter05 exercises

--- src/chapters/chapter05/exercise42.java
+++ src/chapters/chapter05/exercise42.java
@@ -1,10 +1,33 @@
 package chapters.chapter05;
 
+import java.util.Scanner;
+
 public class exercise42 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Financial application: find the sales amount.
 
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter commission amount:");
+		double commissionSought = input.nextDouble();
+		double sale = 0, commission = 0;
+
+		while (commission < commissionSought) {
+
+			if (sale <= 5000) {
+				commission = sale * 0.08;
+
+			} else if (sale <= 10000) {
+				commission = 5000 * .08 + (sale - 5000) * 0.1;
+
+			} else {
+
+				commission = 5000 * .08 + 5000 * 0.1 + (sale - 10000) * .12;
+			}
+
+			sale += 0.01; // increase sale by a penny on each loop
+		}
+		System.out.printf("The sale you need to generate is %.2f", (sale - 0.01));
 	}
 
 }
--- src/chapters/chapter05/exercise44.java
+++ src/chapters/chapter05/exercise44.java
@@ -1,9 +1,30 @@
 package chapters.chapter05;
 
+import java.util.Scanner;
+
 public class exercise44 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Computer architecture: bit-level operations.
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter an int: ");
+		short n = input.nextShort();
+		String s = "";
+
+		short mask = 0b1; 
+
+		for (int i = 0; i < 16; i++) {
+
+			short bit = (short) (n & mask); 
+			// remainder of
+			// division by 2
+			s = bit + s;
+			n = (byte) (n >> 1); 
+		}
+
+		System.out.println("The bits are ");
+		System.out.println(s);
 
 	}
 

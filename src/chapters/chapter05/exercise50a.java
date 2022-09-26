2a3331b9657ae9afda5eb0d4dce94373cf3d485d
VedatDokur <vedatdokurr7@gmail.com>
2022-09-26 23:49:55 +0200

    added chapter05 exercises

---
+++ src/chapters/chapter05/exercise50a.java
@@ -0,0 +1,25 @@
+package chapters.chapter05;
+
+import java.util.Scanner;
+
+public class exercise50a {
+
+	public static void main(String[] args) {
+		// Count uppercase letters.
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a string:");
+		String s = input.nextLine();
+		int numUp = 0;
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			if ('A' <= s.charAt(i) && s.charAt(i) <= 'Z') {
+				numUp++;
+			}
+		}
+
+		System.out.println("The number of uppercase letters is " + numUp);
+
+	}
+
+}
---
+++ src/chapters/chapter05/exericse49a.java
@@ -0,0 +1,40 @@
+package chapters.chapter05;
+
+import java.util.Scanner;
+
+public class exericse49a {
+
+	public static void main(String[] args) {
+		// Count vowels and consonants.
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a string:");
+		String s = input.nextLine();
+		String s2 = "";
+		int numVowel = 0;
+		int numCon = 0;
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			switch (s.toLowerCase().charAt(i)) {
+			case 'a':
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u':
+				numVowel++;
+				break;
+			case ' ':
+				break;
+			default:
+				numCon++;
+				break;
+
+			}
+		}
+
+		System.out.println("The number of vowels is " + numVowel);
+		System.out.println("The number of consonants is " + numCon);
+
+	}
+
+}
